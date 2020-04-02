9c3dc2c1ae81e6cef182ed8cc9bbe8e91318e0d8
Jen Merritt <jenjomez@gmail.com>
2020-04-02 17:16:56 +0100

    correct url

--- job-listings-app/src/actions/actions.js
+++ job-listings-app/src/actions/actions.js
@@ -1,14 +1,8 @@
 import { DATA_LOADED } from "../constants/action-types";
 
-// export function getData() {
-//         fetch("http://localhost:3000/api/data.json")
-//         .then(response => response.json())
-//         .then(jobs => console.log(jobs));
-// };
-
 export function getData() {
     return dispatch => {
-        return fetch("http://localhost:3000/api/data.json")
+        return fetch("/api/data.json")
         .then(response => response.json())
         .then(jobs => {
             dispatch({ type: DATA_LOADED, payload: jobs });
